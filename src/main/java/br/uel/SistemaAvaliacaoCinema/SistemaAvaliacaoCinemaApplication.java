03a242582c2a0a04ee846e68f5335054d081072f
RafaelCobo <rafael@outook.com>
2025-11-08 12:21:38 -0300

    diciona estrutura inicial do projeto Spring Boot (JDBC)

---
+++ src/main/java/br/uel/SistemaAvaliacaoCinema/SistemaAvaliacaoCinemaApplication.java
@@ -0,0 +1,13 @@
+package br.uel.SistemaAvaliacaoCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAvaliacaoCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAvaliacaoCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uel/SistemaAvaliacaoCinema/SistemaAvaliacaoCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uel.SistemaAvaliacaoCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAvaliacaoCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
